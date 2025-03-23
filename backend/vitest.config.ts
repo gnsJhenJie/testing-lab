6b5b699174c1a621689f09ec1b339507fe1c7fa8
Jhen-Jie Hsieh <gnsjhenjie.cs13@nycu.edu.tw>
2025-03-23 15:56:14 +0000

    chore: Update vitest config to show coverage report on failure

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    pool: 'forks'
+    pool: 'forks',
+    coverage: {
+      reportOnFailure: true
+    }
   }
 })
