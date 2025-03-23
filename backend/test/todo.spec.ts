4344f304afbdb86f4bc633255389c1c57174878b
Jhen-Jie Hsieh <gnsjhenjie.cs13@nycu.edu.tw>
2025-03-23 15:57:47 +0000

    test: Add test on Todo API

--- backend/test/todo.spec.ts
+++ backend/test/todo.spec.ts
@@ -23,4 +23,33 @@ describe('Todo API Testing', () => {
     const todos = JSON.parse(response.body)['todos']
     expect(todos).toStrictEqual([])
   })
+
+  test('Given an todos array return from repo function, When send a GET /api/v1/todos, Then it should return a list of todos with status code 200', async () => {
+    const fakeTodos = [{ id: '1', name: 'Mom', description: 'Buy Tomatoes, Banana, Meat', status: false }]
+    vi.spyOn(TodoRepo, 'findAllTodos').mockResolvedValue(fakeTodos)
+
+    const response = await server.inject({
+      method: 'GET',
+      url: '/api/v1/todos'
+    })
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual({ todos: fakeTodos })
+
+  })
+
+  // test('When send a DELETE /api/v1/todos/:id with non-existed id, Then it should return status code 404 when todo is not found', async () => {
+
+  //   vi.spyOn(TodoRepo, 'deleteTodoById').mockResolvedValue()
+  //   const fakeId = '1'
+
+  //   const response = await server.inject({
+  //     method: 'DELETE',
+  //     url: `/api/v1/todos/${fakeId}`
+  //   })
+
+  //   expect(response.statusCode).toBe(404)
+  //   expect(response.json()).toEqual({ msg: `Not Found Todo:${fakeId}` })
+  // })
+
 })
